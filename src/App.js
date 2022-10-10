6a43da958e305b090b9a9a3482f212406ca659ff
Jabir Omar <jabiromar400@gmail.com>
2022-10-10 11:07:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Form from './components/form/Form';
+// import NavBar from './components/navBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <NavBar /> */}
+    <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+
+function NavBar() {
+  return (
+    <nav>
+        <div id='right_nav'>
+            <p>Login Form</p>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
